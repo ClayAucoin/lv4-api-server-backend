a6ebf2328260172129fc1853e04c4771d3b34cba
ClayAucoin <clay0aucoin@gmail.com>
2025-11-23 12:46:42 -0600

    finish setup and got root running

---
+++ src/app.js
@@ -0,0 +1,50 @@
+// src/index.js
+import express from "express"
+import cors from "cors"
+import { sendError } from "./utils/sendError.js"
+
+// import routes
+import rootRouter from "./routes/root.js"
+
+const app = express();
+// const port = 3000;
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// use routes
+app.use("/", rootRouter)
+
+export function globalErrorHandler(err, req, res, next) {
+  const status = err.status || 500
+  const code = err.code || "INTERNAL_ERROR"
+  const message = err.message || "Server error"
+
+  const payload = {
+    ok: false,
+    error: {
+      status,
+      message,
+      code
+    }
+  }
+
+  if (err.details) {
+    payload.error.details = err.details
+  }
+
+  res.status(status).json(payload)
+}
+
+export function error404(req, res, next) {
+  next(sendError(404, "Route not found", "NOT_FOUND"))
+}
+
+// routes error 404
+app.use(error404)
+
+// global error handling
+app.use(globalErrorHandler)
+
+export default app
